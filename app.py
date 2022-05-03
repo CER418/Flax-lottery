9d4deab4eeb8de11d78867472287e2f6a60ab312
Martin Fidjeland <cer418@protonmail.com>
2022-05-03 18:03:23 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 from collections import Counter
 from flask import Flask, render_template, request
 from PIL import Image
@@ -7,13 +8,13 @@ from pytesseract import pytesseract
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/images'
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
-app.secret_key = 'en173Tk8xfXSnOzu5vjjyNf0YfWH2EC4'
+app.secret_key = os.getenv('secret_key')
 
 possible_numbers = [25, 50, 100, 250, 500, 1000, 10_000, 100_000, 1_000_000]
 
 
 def allowed_image(filename):
-    if not "." in filename:
+    if "." not in filename:
         return False
     ext = filename.rsplit(".", 1)[1]
     if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
@@ -40,19 +41,16 @@ def check_numbers(lst):
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            error = 'No file part.'
-            return render_template("index.html", error=error)
+            return render_template("index.html", error='No file part.')
         file = request.files['file']
         if file.filename == '':
-            error = 'No selected file.'
-            return render_template("index.html", error=error)
+            return render_template("index.html", error='No selected file.')
         if allowed_image(file.filename):
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], "image.jpg"))
             if check_numbers(return_numbers("static/images/image.jpg")):
                 message = "This may be a winning ticket!"
             else:
                 message = "Doesn't look like it.."
-
             os.remove("static/images/image.jpg")
             return render_template("index.html", message=message)
         else:
